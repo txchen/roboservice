042d237c83725bc3f6c8c5d2121e5093d36c72fa
Tianxiang Chen <txchendev@gmail.com>
2015-12-20 22:02:04 -0800

    add a simple couting logic

--- app/src/main/java/com/tc/roboservice/CountingService.java
+++ app/src/main/java/com/tc/roboservice/CountingService.java
@@ -1,31 +1,89 @@
 package com.tc.roboservice;
 
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Handler;
 import android.os.IBinder;
+import android.os.Message;
+import android.provider.SyncStateContract;
+import android.support.v7.app.NotificationCompat;
 import android.util.Log;
 import android.widget.Toast;
 
 public class CountingService extends Service {
 
     private int mCurrentScore = 0;
+    private final Handler myHandler = new Handler();
 
     private final String TAG = "CS";
 
     @Override
     public void onCreate() {
+        super.onCreate();
         mCurrentScore = 0;
         Log.i(TAG, "onCreate CountingService");
+        CountThread ct = new CountThread();
+        ct.start();
+    }
+
+    private Notification buildNotification(String text) {
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0,
+                new Intent(this, MainActivity.class), 0);
+        Bitmap icon = BitmapFactory.decodeResource(getResources(),
+                R.mipmap.ic_launcher);
+
+        return new NotificationCompat.Builder(this)
+                .setContentTitle("RoboService")
+                .setTicker("RoboService started!!!")
+                .setContentText(text)
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setLargeIcon(
+                        Bitmap.createScaledBitmap(icon, 128, 128, false))
+                .setContentIntent(pendingIntent)
+                .setOngoing(true)
+                .build();
     }
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
         Log.i(TAG, "onStartCommand CountingService");
         Toast.makeText(this, "CountingService is starting", Toast.LENGTH_SHORT).show();
+        startForeground(101, buildNotification("My Text"));
 
         // If we get killed, after returning from here, restart
         return START_STICKY;
     }
+
+    private class CountThread extends Thread {
+        @Override
+        public void run() {
+            super.run();
+            while(true) {
+                try {
+                    sleep(3000);
+                    mCurrentScore++;
+                    myHandler.post(updateRunnable);
+                } catch (Exception e) {
+                    e.getMessage();
+                }
+            }
+        }
+    }
+
+    final Runnable updateRunnable = new Runnable() {
+        public void run() {
+            NotificationManager mNotificationManager =
+                    (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+            mNotificationManager.notify(101, buildNotification("Count: " + Integer.toString(mCurrentScore)));
+        }
+    };
+
 
     @Override
     public IBinder onBind(Intent intent) {
@@ -34,8 +92,15 @@ public class CountingService extends Service {
     }
 
     @Override
-    public void onDestroy() {
+    public void onLowMemory() {
+        super.onLowMemory();
+        Log.w(TAG, "onLowMemory countingService");
+        Toast.makeText(this, "CountingService low memory", Toast.LENGTH_SHORT).show();
+    }
 
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
         Log.w(TAG, "onDestroy countingService");
         Toast.makeText(this, "CountingService is destroyed", Toast.LENGTH_SHORT).show();
     }
